7879ee0dd4a768bc8694ecbb3ca10f63db272702
Jacob Francois <jafranswa@gmail.com>
2024-04-17 11:29:35 -0600

    formatting cleanup

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import { counterActions } from '../store/index';
-import classes from './Counter.module.css';
+import { counterActions } from "../store/index";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counterState = useSelector(state => state.counter);
-  const showCounterState = useSelector(state => state.showCounter);
+  const counterState = useSelector((state) => state.counter);
+  const showCounterState = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
@@ -17,22 +17,22 @@ const Counter = () => {
   };
 
   const increaseHandler = () => {
-    dispatch(counterActions.increase(5))
-  }
+    dispatch(counterActions.increase(5));
+  };
 
   const toggleCounterHandler = () => {
-    dispatch(counterActions.toggleCounter())
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       {showCounterState && <div className={classes.value}>{counterState}</div>}
-          <div>
-            <button onClick={incrementHandler}>Increment</button>
-            <button onClick={increaseHandler}>Increase By 5</button>
-            <button onClick={decrementHandler}>Decrement</button>
-          </div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase By 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,30 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
-        increment(state) {
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        }
-    }
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
 });
 
 const store = configureStore({
-    reducer: counterSlice.reducer
-    });
-    
+  reducer: counterSlice.reducer,
+});
+
 /* if you have multiple reducers a reducer object can
    can be passed to store config
 const store = configureStore({
@@ -36,4 +36,3 @@ const store = configureStore({
 export const counterActions = counterSlice.actions;
 
 export default store;
-
